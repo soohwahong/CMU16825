6d3f292c76a0534d95f8008fef6fb7dec9ee0fcc
soohwahong <soohwahong@gmail.com>
2023-07-05 12:22:11 -0400

    added dataset location for colab

--- assignment2/dataset_location.py
+++ assignment2/dataset_location.py
@@ -1,5 +1,5 @@
 # specify the root location where u downloaded the dataset
-root_location = ""
+root_location = "/content/gdrive/MyDrive/2023Spring/data_shapenet"
 R2N2_PATH = f"{root_location}/r2n2_shapenet_dataset/r2n2"
 SHAPENET_PATH = f"{root_location}/r2n2_shapenet_dataset/shapenet"
 SPLITS_PATH = f"{root_location}/r2n2_shapenet_dataset/split.json"
