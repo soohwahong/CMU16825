f64e9337eab28739b43250647f3c5c0125825189
soohwahong <soohwahong@gmail.com>
2023-07-20 09:46:13 -0400

    try implicit

--- assignment2/model.py
+++ assignment2/model.py
@@ -14,6 +14,51 @@ class ReshapeLayer(nn.Module):
 
     def forward(self, x):
         return x.reshape(self.shape)
+    
+# from https://github.com/autonomousvision/occupancy_networks/blob/master/im2mesh/layers.py#L6
+class ResnetBlockFC(nn.Module):
+    ''' Fully connected ResNet Block class.
+
+    Args:
+        size_in (int): input dimension
+        size_out (int): output dimension
+        size_h (int): hidden dimension
+    '''
+
+    def __init__(self, size_in, size_out=None, size_h=None):
+        super().__init__()
+        # Attributes
+        if size_out is None:
+            size_out = size_in
+
+        if size_h is None:
+            size_h = min(size_in, size_out)
+
+        self.size_in = size_in
+        self.size_h = size_h
+        self.size_out = size_out
+        # Submodules
+        self.fc_0 = nn.Linear(size_in, size_h)
+        self.fc_1 = nn.Linear(size_h, size_out)
+        self.actvn = nn.ReLU()
+
+        if size_in == size_out:
+            self.shortcut = None
+        else:
+            self.shortcut = nn.Linear(size_in, size_out, bias=False)
+        # Initialization
+        nn.init.zeros_(self.fc_1.weight)
+
+    def forward(self, x):
+        net = self.fc_0(self.actvn(x))
+        dx = self.fc_1(self.actvn(net))
+
+        if self.shortcut is not None:
+            x_s = self.shortcut(x)
+        else:
+            x_s = x
+
+        return x_s + dx
 
 class SingleViewto3D(nn.Module):
     def __init__(self, args):
@@ -28,8 +73,6 @@ class SingleViewto3D(nn.Module):
         if args.type == "vox":
             # Input: b x 512
             # Output: b x 1 x 32 x 32 x 32
-            # pass
-            # TODO:
 
             # g - did not work
             # self.decoder = nn.Sequential(
@@ -39,46 +82,46 @@ class SingleViewto3D(nn.Module):
             #     nn.Sigmoid()  # Apply sigmoid activation to ensure voxel values between 0 and 1
             # )
 
-            # self.decoder = nn.Sequential(
-            #     nn.Linear(512, 2048),
-            #     ReshapeLayer((-1, 256, 2, 2, 2)),
-            #     nn.ConvTranspose3d(256, 128, kernel_size=4, stride=2, bias=False, padding=1),
-            #     nn.BatchNorm3d(128),
-            #     nn.ReLU(),
-            #     nn.ConvTranspose3d(128, 64, kernel_size=4, stride=2, bias=False, padding=1),
-            #     nn.BatchNorm3d(64),
-            #     nn.ReLU(),
-            #     nn.ConvTranspose3d(64, 32, kernel_size=4, stride=2, bias=False, padding=1),
-            #     nn.BatchNorm3d(32),
-            #     nn.ReLU(),
-            #     nn.ConvTranspose3d(32, 8, kernel_size=4, stride=2, bias=False, padding=1),
-            #     nn.BatchNorm3d(8),
-            #     nn.ReLU(),
-            #     nn.ConvTranspose3d(8, 1, kernel_size=1, bias=False),
-            #     nn.Sigmoid()
-            # )   
+            self.decoder = nn.Sequential(
+                nn.Linear(512, 2048),
+                ReshapeLayer((-1, 256, 2, 2, 2)),
+                nn.ConvTranspose3d(256, 128, kernel_size=4, stride=2, bias=False, padding=1),
+                nn.BatchNorm3d(128),
+                nn.ReLU(),
+                nn.ConvTranspose3d(128, 64, kernel_size=4, stride=2, bias=False, padding=1),
+                nn.BatchNorm3d(64),
+                nn.ReLU(),
+                nn.ConvTranspose3d(64, 32, kernel_size=4, stride=2, bias=False, padding=1),
+                nn.BatchNorm3d(32),
+                nn.ReLU(),
+                nn.ConvTranspose3d(32, 8, kernel_size=4, stride=2, bias=False, padding=1),
+                nn.BatchNorm3d(8),
+                nn.ReLU(),
+                nn.ConvTranspose3d(8, 1, kernel_size=1, bias=False),
+                nn.Sigmoid()
+            )   
 
-            # a -
-            in_channels = 64
-            channels = [32, 16, 8]
-            modules = []
-            for mid_channels in channels:
-                modules += [
-                    nn.ConvTranspose3d(in_channels=in_channels, out_channels=mid_channels, 
-                                       kernel_size=4, stride=2, padding=1),
-                    nn.BatchNorm3d(mid_channels),
-                    nn.ReLU()
-                ]
-                in_channels = mid_channels
+            # # a -
+            # in_channels = 64
+            # channels = [32, 16, 8]
+            # modules = []
+            # for mid_channels in channels:
+            #     modules += [
+            #         nn.ConvTranspose3d(in_channels=in_channels, out_channels=mid_channels, 
+            #                            kernel_size=4, stride=2, padding=1),
+            #         nn.BatchNorm3d(mid_channels),
+            #         nn.ReLU()
+            #     ]
+            #     in_channels = mid_channels
 
-            # output layer
-            out_channels = 1
-            modules += [
-                nn.ConvTranspose3d(in_channels=in_channels, out_channels=out_channels,
-                                   kernel_size=4, stride=2, padding=1),
-                nn.BatchNorm3d(out_channels),
-            ]
-            self.decoder = torch.nn.Sequential(*modules)
+            # # output layer
+            # out_channels = 1
+            # modules += [
+            #     nn.ConvTranspose3d(in_channels=in_channels, out_channels=out_channels,
+            #                        kernel_size=4, stride=2, padding=1),
+            #     nn.BatchNorm3d(out_channels),
+            # ]
+            # self.decoder = torch.nn.Sequential(*modules)
 
         elif args.type == "point":
             # Input: b x 512
@@ -103,7 +146,30 @@ class SingleViewto3D(nn.Module):
             self.decoder = nn.Sequential(
                 nn.Linear(512, 3*mesh_pred.verts_packed().shape[0]),
                 nn.Tanh()
-            )              
+            )
+
+        elif args.type == 'implicit':
+            # Input : b x (512 + num_coords x 3)
+            # Output : b x num_coords x 1
+            
+            # Start with a simple implementation of a network that predicts the occupancy given the image feature and a 3d coordinate as input. 
+            # You will need to create a meshgrid of 32x32x32 in the normalized coordinate space of (-1,1)^3 to predict the full occupancy output.
+            # It does not perform any form of normalization.
+            
+
+            self.decoder =  nn.Sequential(
+                ResnetBlockFC(128, 128, size_h=None),
+                ResnetBlockFC(128, 128, size_h=None),
+                ResnetBlockFC(128, 128, size_h=None),
+                ResnetBlockFC(128, 128, size_h=None),
+                ResnetBlockFC(128, 128, size_h=None),
+                nn.ReLU(),
+                nn.Linear(128, 1),
+            )
+            # c_dim = 512, dim = 3
+            # self.fc_p = nn.Linear(3, hidden_size)
+            # self.fc_c = nn.Linear(512, hidden_size)
+            # ResnetBlockFC(hidden_size)
 
     def forward(self, images, args):
         results = dict()
@@ -122,20 +188,34 @@ class SingleViewto3D(nn.Module):
         # call decoder
         ## check output size before moving forward!!
         if args.type == "vox":
-            # TODO:
-            # voxels_pred = self.decoder(encoded_feat)   
+            voxels_pred = self.decoder(encoded_feat)   
             # a - 
-            voxels_pred = self.decoder(encoded_feat.reshape((encoded_feat.shape[0], 64, 2, 2, 2)))         
+            # voxels_pred = self.decoder(encoded_feat.reshape((encoded_feat.shape[0], 64, 2, 2, 2)))         
             return voxels_pred
 
         elif args.type == "point":
-            # TODO:
             pointclouds_pred =  self.decoder(encoded_feat)   # 2, 5000, 3
             return pointclouds_pred
 
         elif args.type == "mesh":
-            # TODO:
             deform_vertices_pred = self.decoder(encoded_feat)             
             mesh_pred = self.mesh_pred.offset_verts(deform_vertices_pred.reshape([-1,3]))
             return  mesh_pred          
+        
+        elif args.type == 'implicit':
+            # p : sampled points  (b x num_coords x 3)
+            # c : conditioning input (2D image) (b x 512)
+            # z : latent code? 
 
+            self.n_coords = args.n_coords
+            self.fc_p = nn.Linear(3*self.n_coords, 128)
+            self.fc_c = nn.Linear(512, 128)
+
+            net = self.fc_p(p)
+            net_c = self.fc_c(encoded_feat).unsqueeze(1)
+
+            net = net + net_c
+
+            implicit_pred = self.decoder(net).squeeze(-1)
+
+            return implicit_pred
